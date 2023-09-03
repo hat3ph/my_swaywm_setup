ba1c796f233577e3f516df1d907d29fc6cc7aac8
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-09-03 10:13:26 +0800

    add battery status and improve network info

--- config/sway/scripts/swaybar.sh
+++ config/sway/scripts/swaybar.sh
@@ -5,7 +5,7 @@ cpu_emoji=🍟
 mem_emoji=🥓
 date_emoji=🕘
 #net_emoji=🌐
-net_emoji=📡
+#net_emoji=📡
 
 # current date
 #date_formatted=$(date "+%a %F %H:%M")
@@ -26,8 +26,26 @@ cpu_usage="$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%
 # check total memory usage
 mem_usage="$(free | grep Mem | awk '{printf "%2.0f%%\n", $3/$2 * 100}')"
 
-#net_info="$(ip -o route get to 8.8.8.8 | awk '{print $5}') : $(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')"
-net_info="$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p') ($(ip -o route get to 8.8.8.8 | awk '{print $5}'))"
+# check network connection
+#net_info="$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p') ($(ip -o route get to 8.8.8.8 | awk '{print $5}'))"
+ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && net_info="$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p') ($(ip -o route get to 8.8.8.8 | awk '{print $5}'))" net_emoji=🛜 || net_info="Disconnected" net_emoji=⛔
+
+# Returns the battery status: "Full", "Discharging", or "Charging".
+if [[ -d /sys/class/power_supply/BAT0 ]]; then
+	battery_status=$(cat /sys/class/power_supply/BAT0/status)
+	if [[ $battery_status == "Discharging" ]] || [[ $battery_status == "Charging" ]]; then
+		battery_emoji=🪫
+	else
+		battery_emoji=🔋
+	fi
+else
+	battery_emoji=🔌
+	battery_status="AC"
+fi
 
 # output to swaybar
-echo $cpu_emoji $cpu_usage $mem_emoji $mem_usage $net_emoji $net_info $volume_emoji $volume_level $date_emoji $date_formatted
+if [[ -d /sys/class/power_supply/BAT0 ]]; then
+	echo $cpu_emoji $cpu_usage $mem_emoji $mem_usage $net_emoji $net_info $battery_emoji $battery_status $volume_emoji $volume_level $date_emoji $date_formatted
+else
+	echo $cpu_emoji $cpu_usage $mem_emoji $mem_usage $net_emoji $net_info $volume_emoji $volume_level $date_emoji $date_formatted
+fi
