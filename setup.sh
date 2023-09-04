63d47f31d67267a8de102aa4421d6c95aac9563d
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-09-04 17:12:21 +0800

    backup current config file first

--- setup.sh
+++ setup.sh
@@ -13,16 +13,20 @@ if [[ $extra_pkgs == "yes" ]]; then
 fi
 
 # copy my swaywm and mako configuration
+if [[ -d $HOME/.config/sway ]]; then mv $HOME/.config/sway $HOME/.config/sway_`date +%Y_%d_%m_%H_%M_%S`; fi
+if [[ -d $HOME/.config/mako ]]; then mv $HOME/.config/mako $HOME/.config/mako`date +%Y_%d_%m_%H_%M_%S`; fi
 mkdir -p $HOME/.config/{sway,mako}
 cp -r ./config/sway/* $HOME/.config/sway/
 chmod +x $HOME/.config/sway/scripts/*.sh
 cp ./config/mako/config $HOME/.config/mako/
 
 # configure nano with line number
+if [[ -f $HOME/.nanorc ]]; then mv $HOME/.nanorc $HOME/.nanorc_`date +%Y_%d_%m_%H_%M_%S`; fi
 cp /etc/nanorc $HOME/.nanorc
 sed -i 's/# set const/set const/g' $HOME/.nanorc
 
 # enable firefox to run in wayland protocol
+if [[ -f $HOME/.profile ]]; then mv $HOME/.profile $HOME/.profile_`date +%Y_%d_%m_%H_%M_%S`; fi
 if [[ ! $(cat $HOME/.profile | grep "^[^#;]" | grep MOZ_ENABLE_WAYLAND=1 ) ]]; then
     printf "\nexport MOZ_ENABLE_WAYLAND=1\n"
 fi
