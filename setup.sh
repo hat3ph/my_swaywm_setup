713aa6e52a657c20df44ea5dc8ebb831c505854b
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-09-07 08:06:02 +0800

    disable networking.service while using network-manager in debian

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ install () {
 	sudo apt-get install sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland foot suckless-tools \
 		fonts-noto-color-emoji fonts-font-awesome mako-notifier libnotify-bin grim imagemagick nano less iputils-ping -y
 
-  	# use pipewire with wireplumber or pulseaudio-utils
+	# use pipewire with wireplumber or pulseaudio-utils
 	if [[ $audio == "yes" ]]; then
 		if [[ $wireplumber == "yes" ]]; then
 			sudo apt-get install pipewire pipewire-pulse wireplumber -y
@@ -24,30 +24,31 @@ install () {
 		fi
 	fi
 
- 	# optional install thunar and extra packages
+	# optional install thunar and extra packages
 	if [[ $thunar == "yes" ]]; then
 		sudo apt-get install thunar gvfs gvfs-backends thunar-archive-plugin thunar-media-tags-plugin avahi-daemon -y
 	fi
 
- 	# optional install NetworkManager
+	# optional install NetworkManager
 	if [[ $nm == yes ]]; then
 	sudo apt-get install network-manager -y
 		if [[ -n "$(uname -a | grep Ubuntu)" ]]; then
 			for file in `find /etc/netplan/* -maxdepth 0 -type f -name *.yaml`; do
-  				sudo mv $file $file.bak
+				sudo mv $file $file.bak
 			done
 			echo -e "# Let NetworkManager manage all devices on this system\nnetwork:\n  version: 2\n  renderer: NetworkManager" | \
-            	sudo tee /etc/netplan/01-network-manager-all.yaml
+				sudo tee /etc/netplan/01-network-manager-all.yaml
 		else
 			sudo cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
 			sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf
-        	sudo mv /etc/network/interfaces /etc/network/interfaces.bak
-        	sudo cp ./config/interfaces /etc/network/interfaces
+			sudo mv /etc/network/interfaces /etc/network/interfaces.bak
+			sudo cp ./config/interfaces /etc/network/interfaces
+			sudo systemctl disable networking.service
 		fi
 	fi
 
- 	# copy my swaywm and mako configuration
-  	if [[ $my_swaywm_config == "yes" ]]; then
+	# copy my swaywm and mako configuration
+	if [[ $my_swaywm_config == "yes" ]]; then
 		if [[ -d $HOME/.config/sway ]]; then mv $HOME/.config/sway $HOME/.config/sway_`date +%Y_%d_%m_%H_%M_%S`; fi
 		if [[ -d $HOME/.config/mako ]]; then mv $HOME/.config/mako $HOME/.config/mako`date +%Y_%d_%m_%H_%M_%S`; fi
 		mkdir -p $HOME/{Documents,Downloads,Music,Pictures,Videos}
@@ -55,16 +56,16 @@ install () {
 		cp -r ./config/sway/* $HOME/.config/sway/
 		chmod +x $HOME/.config/sway/scripts/*.sh
 		cp ./config/mako/config $HOME/.config/mako/
- 	fi
+	fi
 
- 	# configure nano with line number
+	# configure nano with line number
 	if [[ $nano_config == "yes" ]]; then
 		if [[ -f $HOME/.nanorc ]]; then mv $HOME/.nanorc $HOME/.nanorc_`date +%Y_%d_%m_%H_%M_%S`; fi
 		cp /etc/nanorc $HOME/.nanorc
 		sed -i 's/# set const/set const/g' $HOME/.nanorc
 	fi
 
- 	# enable firefox to run in wayland protocol
+	# enable firefox to run in wayland protocol
 	if [[ $moz_enable == "yes" ]]; then
 		if [[ -f $HOME/.profile ]]; then cp $HOME/.profile $HOME/.profile_`date +%Y_%d_%m_%H_%M_%S`; fi
 		if [[ ! $(cat $HOME/.profile | grep "^[^#;]" | grep MOZ_ENABLE_WAYLAND=1 ) ]]; then
