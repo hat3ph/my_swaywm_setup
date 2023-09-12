f2d33c7a9322187b987d9d893e76504ef4d21f21
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-09-12 15:30:10 +0800

    fix only exit sway using the power option menu

--- config/sway/scripts/dmenu_power_options.sh
+++ config/sway/scripts/dmenu_power_options.sh
@@ -14,10 +14,10 @@ case "$choice" in
   0) swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' \
     -B 'Yes, exit sway' 'swaymsg exit' & ;;
   1) swaynag -t warning -m 'You pressed the shutdown shortcut. Do you really want to shutdown your system?' \
-    -B 'Yes, systemctl poweroff' 'swaymsg exit' & ;;
+    -B 'Yes, shutdown my system' 'systemctl poweroff' & ;;
   2) swaynag -t warning -m 'You pressed the reboot shortcut. Do you really want to reboot your system?' \
-    -B 'Yes, systemctl reboot' 'swaymsg exit' & ;;
+    -B 'Yes, reboot my system' 'systemctl reboot' & ;;
   3) swaynag -t warning -m 'You pressed the suspend shortcut. Do you really want to suspend your system?' \
-    -B 'Yes, systemctl suspend' 'swaymsg exit' & ;;
+    -B 'Yes, suspend my system' 'systemctl suspend' & ;;
   4) exit ;;
 esac
