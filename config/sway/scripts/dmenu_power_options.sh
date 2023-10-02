f71d3b359695c893151acd82c676301556b06eb0
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-10-02 14:45:16 +0800

    Shorten the confirmation message

--- config/sway/scripts/dmenu_power_options.sh
+++ config/sway/scripts/dmenu_power_options.sh
@@ -12,12 +12,12 @@ case "$choice" in
   #2) systemctl suspend & ;;
   #3) systemctl reboot & ;;
   0) swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' \
-    -B 'Yes, exit sway' 'swaymsg exit' & ;;
+    -B 'Exit' 'swaymsg exit' & ;;
   1) swaynag -t warning -m 'You pressed the shutdown shortcut. Do you really want to shutdown your system?' \
-    -B 'Yes, shutdown my system' 'systemctl poweroff' & ;;
+    -B 'Shutdown' 'systemctl poweroff' & ;;
   2) swaynag -t warning -m 'You pressed the reboot shortcut. Do you really want to reboot your system?' \
-    -B 'Yes, reboot my system' 'systemctl reboot' & ;;
+    -B 'Reboot' 'systemctl reboot' & ;;
   3) swaynag -t warning -m 'You pressed the suspend shortcut. Do you really want to suspend your system?' \
-    -B 'Yes, suspend my system' 'systemctl suspend' & ;;
+    -B 'Suspend' 'systemctl suspend' & ;;
   4) exit ;;
 esac
